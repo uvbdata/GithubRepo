962861cbf2cdc69fcd094c1a2e4a9705997c3aad
Thorsten Boldin <uvbdata@gmail.com>
2016-10-18 10:09:41 -0600

    Initial Commit

---
+++ GithubRepo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GithubRepo
+//
+//  Created by Thorsten Boldin on 18/10/2016.
+//  Copyright © 2016 Thorsten Boldin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
